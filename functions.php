b5fb089c81bbf8bf4029c6bd8c406c0a46e1dc9f
Mathias <mathias@agence-alchimy.fr>
2023-10-13 11:27:09 +0200

    [STEP 4] add css and js loadings

--- functions.php
+++ functions.php
@@ -5,3 +5,31 @@ add_theme_support('post-thumbnails');
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support('title-tag');
+
+// Chargement des styles et des scripts
+function wpavance_register_assets()
+{
+
+    // Déclarer jQuery
+    wp_enqueue_script('jquery');
+
+    // Déclarer le JS
+    wp_enqueue_script(
+        'wpavance',
+        get_template_directory_uri() . '/js/script.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
+
+    // Déclarer le fichier style.css à la racine du thème
+    wp_enqueue_style(
+        'wpavance',
+        get_stylesheet_uri(),
+        array(),
+        '1.0'
+    );
+
+
+}
+add_action('wp_enqueue_scripts', 'wpavance_register_assets');
