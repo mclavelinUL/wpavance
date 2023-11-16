61869be00e888112f644c942d4d5a64d8b2a9cb3
Mathias <mathias@agence-alchimy.fr>
2023-11-16 12:12:02 +0100

    [STEP 9] add last jeux on front page

--- front-page.php
+++ front-page.php
@@ -11,5 +11,18 @@
         <?php the_content(); ?>
 
     <?php endwhile; endif; ?>
+<?php
+$jeux = get_posts([
+    'post_type' => 'jeux',
+    'numberposts' => 3,
+    'orderby' => 'date',
+    'order' => 'DESC',
 
+]);
+foreach ($jeux as $jeu) {
+    echo '<h2>' . $jeu->post_title . '</h2>';
+    echo '<p>' . $jeu->post_excerpt . '</p>';
+    echo '<a href="' . get_permalink($jeu) . '">Lire la suite</a>';
+}
+?>
 <?php get_footer(); ?>
